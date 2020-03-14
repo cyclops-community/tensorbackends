e65760a0e05f34ec6e3e227dd303d2a28b0ee37c
Yuchen Pang <32781495+yuchenpang@users.noreply.github.com>
2020-03-14 15:58:24 -0500

    Improve function delegating mechanism
    1. AttributeError are raised when an attribute is not found
    2. Other exceptions will be thrown directly instead of being caught and thrown

--- tensorbackends/backends/ctfview/ctfview_backend.py
+++ tensorbackends/backends/ctfview/ctfview_backend.py
@@ -147,28 +147,28 @@ class CTFViewBackend(Backend):
         unwrap = lambda val: val.unwrap() if isinstance(val, CTFViewTensor) else val
         try:
             result = getattr(ctf, attr)
-            if callable(result):
-                def wrapped_result(*args, **kwargs):
-                    unwrapped_args = tuple(unwrap(v) for v in args)
-                    unwrapped_kwargs = {k: unwrap(v) for k, v in kwargs.items()}
-                    retval = result(*unwrapped_args, **unwrapped_kwargs)
-                    if isinstance(retval, tuple):
-                        wrapped_retval = tuple(wrap(v) for v in retval)
-                    elif isinstance(retval, list):
-                        wrapped_retval = [wrap(v) for v in retval]
-                    elif isinstance(retval, dict):
-                        wrapped_retval = {k: wrap(v) for k, v in retval.items()}
-                    else:
-                        wrapped_retval = wrap(retval)
-                    return wrapped_retval
-                wrapped_result.__module__ = type(self).__module__
-                wrapped_result.__name__ = attr
-                wrapped_result.__qualname__ = '{}.{}'.format(type(self).__qualname__, attr)
-                return wrapped_result
-            else:
-                return result
-        except Exception as e:
-            raise ValueError('failed to get {} from ctf'.format(attr)) from e
+        except AttributeError as e:
+            raise AttributeError("failed to get '{}' from ctf".format(attr)) from e
+        if callable(result):
+            def wrapped_result(*args, **kwargs):
+                unwrapped_args = tuple(unwrap(v) for v in args)
+                unwrapped_kwargs = {k: unwrap(v) for k, v in kwargs.items()}
+                retval = result(*unwrapped_args, **unwrapped_kwargs)
+                if isinstance(retval, tuple):
+                    wrapped_retval = tuple(wrap(v) for v in retval)
+                elif isinstance(retval, list):
+                    wrapped_retval = [wrap(v) for v in retval]
+                elif isinstance(retval, dict):
+                    wrapped_retval = {k: wrap(v) for k, v in retval.items()}
+                else:
+                    wrapped_retval = wrap(retval)
+                return wrapped_retval
+            wrapped_result.__module__ = type(self).__module__
+            wrapped_result.__name__ = attr
+            wrapped_result.__qualname__ = '{}.{}'.format(type(self).__qualname__, attr)
+            return wrapped_result
+        else:
+            return result
 
     def _einsum(self, expr, operands):
         inputs_indices = [operand.indices for operand in operands]
--- tensorbackends/backends/numpy/numpy_backend.py
+++ tensorbackends/backends/numpy/numpy_backend.py
@@ -138,28 +138,28 @@ class NumPyBackend(Backend):
         unwrap = lambda val: val.unwrap() if isinstance(val, NumPyTensor) else val
         try:
             result = getattr(np, attr) if hasattr(np, attr) else getattr(la, attr)
-            if callable(result):
-                def wrapped_result(*args, **kwargs):
-                    unwrapped_args = tuple(unwrap(v) for v in args)
-                    unwrapped_kwargs = {k: unwrap(v) for k, v in kwargs.items()}
-                    retval = result(*unwrapped_args, **unwrapped_kwargs)
-                    if isinstance(retval, tuple):
-                        wrapped_retval = tuple(wrap(v) for v in retval)
-                    elif isinstance(retval, list):
-                        wrapped_retval = [wrap(v) for v in retval]
-                    elif isinstance(retval, dict):
-                        wrapped_retval = {k: wrap(v) for k, v in retval.items()}
-                    else:
-                        wrapped_retval = wrap(retval)
-                    return wrapped_retval
-                wrapped_result.__module__ = type(self).__module__
-                wrapped_result.__name__ = attr
-                wrapped_result.__qualname__ = '{}.{}'.format(type(self).__qualname__, attr)
-                return wrapped_result
-            else:
-                return result
-        except Exception as e:
-            raise ValueError('failed to get {} from numpy'.format(attr)) from e
+        except AttributeError as e:
+            raise AttributeError("failed to get '{}' from numpy or numpy.linalg".format(attr)) from e
+        if callable(result):
+            def wrapped_result(*args, **kwargs):
+                unwrapped_args = tuple(unwrap(v) for v in args)
+                unwrapped_kwargs = {k: unwrap(v) for k, v in kwargs.items()}
+                retval = result(*unwrapped_args, **unwrapped_kwargs)
+                if isinstance(retval, tuple):
+                    wrapped_retval = tuple(wrap(v) for v in retval)
+                elif isinstance(retval, list):
+                    wrapped_retval = [wrap(v) for v in retval]
+                elif isinstance(retval, dict):
+                    wrapped_retval = {k: wrap(v) for k, v in retval.items()}
+                else:
+                    wrapped_retval = wrap(retval)
+                return wrapped_retval
+            wrapped_result.__module__ = type(self).__module__
+            wrapped_result.__name__ = attr
+            wrapped_result.__qualname__ = '{}.{}'.format(type(self).__qualname__, attr)
+            return wrapped_result
+        else:
+            return result
 
     def _einsum(self, expr, operands):
         result = np.einsum(expr.indices_string, *(operand.tsr for operand in operands), optimize='greedy')
